008e303a09abc7092d2c92f90a820656635420d1
Kolentsionok <meliciron@mail.ru>
2021-09-21 17:58:05 +0300

    first commit

---
+++ js/script.js
@@ -0,0 +1,49 @@
+const animItems = document.querySelectorAll("._anim-items");
+
+if (animItems.length > 0) {
+  window.addEventListener("scroll", animOnScroll);
+  function animOnScroll() {
+    for (let i = 0; i < animItems.length; i++) {
+      const animItem = animItems[i];
+      const animItemHeight = animItem.offsetHeight; // высота объекта
+      const animItemOffset = offset(animItem).top;
+      const animStart = 4;
+
+      let animItemPoint = window.innerHeight - animItemHeight / animStart;
+      if (animItemHeight > window.innerHeight) {
+        animItemPoint = window.innerHeight - window.innerHeight / animStart;
+      }
+
+      if (
+        window.pageYOffset > animItemOffset - animItemPoint &&
+        window.pageYOffset < animItemOffset + animItemHeight
+      ) {
+        animItem.classList.add("_active");
+      } else {
+        animItem.classList.remove("_active");
+      }
+    }
+  }
+  function offset(el) {
+    const rect = el.getBoundingClientRect(),
+      scrollLeft = window.pageXOffset || document.documentElement.scrollLeft,
+      scrollTop = window.pageYOffset || document.documentElement.scrollTop;
+    return { top: rect.top + scrollTop, left: rect.left + scrollLeft };
+  }
+  animOnScroll();
+}
+
+$(document).ready(function () {
+  $(".slider").slick({
+    dots: true,
+    speed: 1000,
+    easing: "easeOut",
+    initialSlide: 0,
+    slidesToShow: 3,
+    autoplay: false,
+    autoplaySpeed: 1500,
+    waitForAnimate: true,
+    centerMode: true,
+    infinite: true,
+  });
+});
